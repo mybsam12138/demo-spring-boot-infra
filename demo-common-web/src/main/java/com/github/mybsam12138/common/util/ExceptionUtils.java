62fb4c8da5b7e2be5bd0d33fd1da4037227c4894
Sam <mybsam12@gmail.com>
2026-01-30 15:59:38 +0800

    add Exceptionutils to support get rootcause to help handle exception

---
+++ demo-common-web/src/main/java/com/github/mybsam12138/common/util/ExceptionUtils.java
@@ -0,0 +1,34 @@
+package com.github.mybsam12138.common.util;
+
+public final class ExceptionUtils {
+
+    private ExceptionUtils() {}
+
+    public static Throwable getRootCause(Throwable t) {
+        Throwable cause = t;
+        while (cause.getCause() != null) {
+            cause = cause.getCause();
+        }
+        return cause;
+    }
+
+    public static boolean isCausedBy(Throwable t, Class<? extends Throwable> type) {
+        Throwable cause = t;
+        while (cause != null) {
+            if (type.isInstance(cause)) {
+                return true;
+            }
+            cause = cause.getCause();
+        }
+        return false;
+    }
+
+    public static String getStackTrace(Throwable t) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(t).append("\n");
+        for (StackTraceElement e : t.getStackTrace()) {
+            sb.append("\tat ").append(e).append("\n");
+        }
+        return sb.toString();
+    }
+}
